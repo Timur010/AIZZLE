5774f6627b985b035e84ac629710dd86e4115f7c
Timur010 <duakar010@gmail.com>
2023-12-13 00:24:21 +0300

    Initial Commit

---
+++ AIZZLE/AIZZLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIZZLEApp.swift
+//  AIZZLE
+//
+//  Created by Timur Kadiev on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AIZZLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
